9328a63283e09d868c64023b93559340d7af9985
rafaelqcosta <rafaqcosta@gmail.com>
2024-06-27 17:23:20 -0300

    fix: main

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-// Importe o componente
-import ListaDeLegendas from "./components/ListaDeLegendas";
-
-// Exporte o componente para uso externo
-export { ListaDeLegendas };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
