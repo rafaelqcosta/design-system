0e1ce08cdbfe7ec665ac63350ad0b887a694f576
rafaelqcosta <rafaqcosta@gmail.com>
2024-06-28 12:24:48 -0300

    Delete src directory

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// Importar os componentes individuais
-import {ListaDeLegendas} from './components/ListaDeLegendas';
-
-// Exportar os componentes para uso externo
-export { ListaDeLegendas };
