55b3ae5b3bf3b8892c582767365f5e7ea2c7c22e
rafaelqcosta <rafaqcosta@gmail.com>
2024-06-28 10:04:14 -0300

    feat: new project

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Importar os componentes individuais
+import {ListaDeLegendas} from './components/ListaDeLegendas';
+
+// Exportar os componentes para uso externo
+export { ListaDeLegendas };
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+};
